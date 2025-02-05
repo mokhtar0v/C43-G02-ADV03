d32c72f4418901757c14f1bdbe659564e7a8fffc
mokhtar0v <Omar.Mokhtar22@svnu.edu.eg>
2025-02-06 00:05:30 +0200

    Question01

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,9 +2,35 @@
 {
     internal class Program
     {
+        static void ReverseQueue(Queue<int> queue)
+        {
+            Stack<int> stack = new Stack<int>();
+            while (queue.Count > 0)
+            {
+                stack.Push(queue.Dequeue());
+            }
+            while (stack.Count > 0)
+            {
+                queue.Enqueue(stack.Pop());
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question01
+            //Queue<int> queue = new Queue<int>();
+            //queue.Enqueue(1);
+            //queue.Enqueue(2);
+            //queue.Enqueue(3);
+            //queue.Enqueue(4);
+            //queue.Enqueue(5);
+
+            //ReverseQueue(queue);
+            //Console.WriteLine("Reversed Queue: ");
+            //foreach (int q in queue)
+            //{
+            //    Console.WriteLine(q);
+            //} 
+            #endregion
         }
     }
 }
